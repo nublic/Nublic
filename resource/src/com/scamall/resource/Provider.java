be72b5552d5228172513af90b71ea9f20e12c515
David Navarro Estruch <david.navarro@ardinformatica.com>
2010-08-02 01:44:35 +0200

    New project resource with the main abstract class Provider
    The class is roughtly documented, feel free to change any comment.

---
+++ resource/src/com/scamall/resource/Provider.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.scamall.resource;
+
+/** An abstract class for Resource Providers.
+ * 
+ *  All Providers of resources must extend this class. All Providers must be thread-safe
+ * @author David Navarro Estruch
+ *
+ */
+public abstract class Provider {
+
+	protected static String id;
+	
+	/** Request a resource.
+	 * 
+	 * @param app The resource owner app
+	 * @param key The key that the app will use to refer the resource.
+	 * @param args Any number of additional args that a Provider can ask.
+	 */
+	abstract public void request(String app, String key, String[] args);
+
+	/** Gets the value from a given resource by the Provider.
+	 * 
+	 * @param key The key that refers to the resource.
+	 * @param subkey A subkey for an specific data of the resource.
+	 * @return The subkey data of the key resource.
+	 */
+	abstract public String value(String key, String subkey);
+	
+	/** Releases a resource. After a release the resource must not be used again.
+	 * 
+	 * @param key The key that the app use to refer the resource.
+	 * @param args Any number of additional args that a Provider can ask.
+	 */
+	abstract void release(String key, String[] args);
+	
+}
