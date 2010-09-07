2ec54b9d1554d7db9129578c3b854cd15b1ac376
David Navarro Estruch <david@david-netbook.(none)>
2010-09-07 16:53:12 +0200

    Modified notification to become a correct package

---
+++ notification/main/src/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='scamall-notification',
+      version='0.1',
+      description='The Scamall Notification System',
+      author='David Navarro Estruch',
+      author_email='david@ardinformatica.com',
+      url='http://scamall.ardinformatica.com',
+      packages=['scamall.notification'],
+      license='ALL_RIGHTS_RESERVED',
+      long_description='The Scamall Notification System',
+      platforms=['all']
+     )
