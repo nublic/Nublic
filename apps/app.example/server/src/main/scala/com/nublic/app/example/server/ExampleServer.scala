471291d16ee292d9eab1e93cdd30ca1e081a9c90
serras <trupill@gmail.com>
2011-08-28 21:26:42 +0200

    Move shared declaration to the class.

--- apps/app.example/server/src/main/scala/com/nublic/app/example/server/ExampleServer.scala
+++ apps/app.example/server/src/main/scala/com/nublic/app/example/server/ExampleServer.scala
@@ -8,8 +8,9 @@ import net.liftweb.json.Serialization.{read, write}
 class ExampleServer extends ScalatraFilter with JsonSupport {
   // JsonSupport adds the ability to return JSON objects
   
+  implicit val formats = Serialization.formats(NoTypeHints)
+  
   get("/say/:name") {
-    implicit val formats = Serialization.formats(NoTypeHints)
     var greeting = new Greeting("Hello " + params("name") + "!")
     write(greeting)  // write(...) converts an object to JSON
   }
