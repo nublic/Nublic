56ce48711ca0662e3caf07440b06aabab1e0160f
david <davisein@gmail.com>
2011-09-06 20:56:24 +0200

    Included test and some cosmetical changes

---
+++ resource/main/src/nublic/resource/mysql_db_test.py
@@ -0,0 +1,26 @@
+'''
+Created on 06/09/2011
+
+@author: david
+'''
+import unittest
+
+
+class MysqlDBTest(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
