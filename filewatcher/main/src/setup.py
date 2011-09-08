51d61eabc610732f4094acf21330f381e09adee7
serras <trupill@gmail.com>
2011-09-08 16:44:56 +0200

    Debianize file-watcher package.

---
+++ filewatcher/main/src/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='nublic-file-watcher',
+      version='0.1',
+      description='The Nublic File Watcher System',
+      author='Alejandro Serrano Mena',
+      author_email='alex@nublic.com',
+      url='http://nublic.com',
+      packages=['nublic','nublic.filewatcher'],
+      license='ALL_RIGHTS_RESERVED',
+      long_description='The Nublic File Watcher System',
+      platforms=['all']
+     )
