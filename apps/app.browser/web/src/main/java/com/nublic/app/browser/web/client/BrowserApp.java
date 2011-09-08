23dc94492d26c6b66cc3955da3cadb9db20e8397
serras <trupill@gmail.com>
2011-09-08 18:11:39 +0200

    Update GWT dependencies to 2.4.0-rc1

--- apps/app.browser/web/src/main/java/com/nublic/app/browser/web/client/BrowserApp.java
+++ apps/app.browser/web/src/main/java/com/nublic/app/browser/web/client/BrowserApp.java
@@ -1,16 +1,21 @@
 package com.nublic.app.browser.web.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class BrowserApp implements EntryPoint {
 
-  /**
-   * This is the entry point method.
-   */
-  public void onModuleLoad() {
-    
-  }
+	BrowserUi theUi;
+
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+		theUi = new BrowserUi();
+		RootLayoutPanel rp = RootLayoutPanel.get();
+	    rp.add(theUi);
+	}
 }
--- apps/app.browser/web/src/main/java/com/nublic/app/browser/web/client/BrowserUi.java
+++ apps/app.browser/web/src/main/java/com/nublic/app/browser/web/client/BrowserUi.java
@@ -1,13 +1,37 @@
 package com.nublic.app.browser.web.client;
 
+import com.google.gwt.cell.client.TextCell;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.cellview.client.CellTree;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.view.client.AbstractDataProvider;
+import com.google.gwt.view.client.ListDataProvider;
+import com.google.gwt.view.client.NoSelectionModel;
+import com.google.gwt.view.client.SelectionModel.AbstractSelectionModel;
+import com.google.gwt.view.client.TreeViewModel;
 
 public class BrowserUi extends Composite {
 
 	private static BrowserUiUiBinder uiBinder = GWT.create(BrowserUiUiBinder.class);
+	
+	@UiField(provided=true) CellTree folderTree = new CellTree(
+		new TreeViewModel() {
+			final AbstractDataProvider<String> dataProvider = new ListDataProvider<String>();
+			final AbstractSelectionModel<String> selectionModel = new NoSelectionModel<String>();
+			@Override
+			public <T> NodeInfo<?> getNodeInfo(T value) {
+				return new DefaultNodeInfo<String>(dataProvider, new TextCell(), selectionModel, null);
+			}
+			@Override
+			public boolean isLeaf(Object value) {
+				return true;
+			}
+		}, null);
+	@UiField FlowPanel contentPlace;
 
 	interface BrowserUiUiBinder extends UiBinder<Widget, BrowserUi> {
 	}
