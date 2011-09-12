5d7c637d3c329c310d4a6b973d4a8fd6eaa89aaf
serras <trupill@gmail.com>
2011-09-12 17:24:39 +0200

    Make file-watcher daemon work.

--- filewatcher/main/src/nublic/filewatcher/dbus_signals.py
+++ filewatcher/main/src/nublic/filewatcher/dbus_signals.py
@@ -12,10 +12,10 @@ import dbus.service
 Sends signals via D-Bus
 """
 class DbusSignaler(dbus.service.Object):
-    def __init__(self, app_name, loop):
-        dbus.service.Object.__init__(self, dbus.SystemBus(mainloop=loop), '/com/nublic/fielwatcher/' + app_name)
+    def __init__(self, app_name):
+        bus_name = dbus.service.BusName('com.nublic.filewatcher', bus=dbus.SystemBus())
+        dbus.service.Object.__init__(self, bus_name, '/com/nublic/filewatcher/' + app_name)
 
-    @dbus.service.signal(dbus_interface='com.nublic.filewatcher',
-                         signature='sssb')
+    @dbus.service.signal(dbus_interface='com.nublic.filewatcher', signature='sssb')
     def file_changed(self, ty, pathname, src_pathname, isdir):
         print "%s %s" % (ty, pathname)
--- filewatcher/main/src/nublic/filewatcher/watcher.py
+++ filewatcher/main/src/nublic/filewatcher/watcher.py
@@ -6,17 +6,25 @@ Created on 06/09/2011
 @copyright: 2011 Nublic
 '''
 
+import gobject
 import pyinotify
 from dbus_signals import *
 from handler import *
 from manager import *
 
-def start_watching(folder, loop):
+def start_watching(folder):
     wm = WatchManager2()
-    signaler = DbusSignaler('Browser', loop)
+    signaler = DbusSignaler('Browser')
     handler = EventHandler(signaler)
-    notifier = pyinotify.Notifier(wm, handler)
+    notifier = pyinotify.Notifier(wm, handler, timeout=10)
+    gobject.timeout_add(500, quick_check, notifier)
     e_filter = pyinotify.ExcludeFilter(['(/[^/]+)*/\\..*'])
     wdd = wm.add_watch(folder, handler.mask(), rec=True, auto_add=True, exclude_filter=e_filter)
     print "Starting to watch..."
-    notifier.loop()
+
+def quick_check(notifier):
+    notifier.process_events()
+    while notifier.check_events():
+        notifier.read_events()
+        notifier.process_events()
+    return True
