99d41889b6dae28b6f2c882b03d7d1b30dd2f086
david <davisein@gmail.com>
2011-10-17 22:40:26 +0100

    Changes keep going. Improving debian files

---
+++ resource/main/src/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='nublic-resource',
+      version='0.1',
+      description='The Nublic Resource System',
+      author='David Navarro Estruch',
+      author_email='davisein@gmail.com',
+      url='http://nublic.com',
+      packages=['nublic','nublic.resource'],
+      license='ALL_RIGHTS_RESERVED',
+      long_description= \
+"This system provides a common and easy way to access to different resources\n managing the creation, security issues and access to private values.",
+      platforms=['all']
+     )
