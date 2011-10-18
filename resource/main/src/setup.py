bfbe40e25792e890f3c7d0e6d9865333cd988ef8
david <davisein@gmail.com>
2011-10-18 02:47:07 +0100

    Finally fixed a typo and now debuild finishes

--- resource/main/src/setup.py
+++ resource/main/src/setup.py
@@ -3,14 +3,14 @@
 from distutils.core import setup
 
 setup(name='nublic-resource',
-      version='0.1',
+      version='0.01',
       description='The Nublic Resource System',
       author='David Navarro Estruch',
-      author_email='davisein@gmail.com',
+      author_email='david@nublic.com',
       url='http://nublic.com',
       packages=['nublic_resource'],
       license='ALL_RIGHTS_RESERVED',
       long_description= \
-"This system provides a common and easy way to access to different resources\n managing the creation, security issues and access to private values.",
+"This system provides a common and easy way to access to different resources managing the creation, security issues and access to private values.",
       platforms=['all']
      )
