54f7d03fc0dfb5a6f5c3022c315423e5b0127e41
serras <trupill@gmail.com>
2011-10-26 15:57:55 +0200

    [app.music.server] Initial work on tags

--- apps/app.music/server/src/main/scala/com/nublic/app/music/server/MusicServer.scala
+++ apps/app.music/server/src/main/scala/com/nublic/app/music/server/MusicServer.scala
@@ -6,7 +6,10 @@ import org.scalatra.liftjson.JsonSupport
 import net.liftweb.json._
 import net.liftweb.json.Serialization.{read, write}
 import javax.servlet.http.HttpServlet
+import org.squeryl.PrimitiveTypeMode._
 import com.nublic.app.music.server.filewatcher.MusicActor
+import com.nublic.app.music.server.model._
+import java.lang.Long
 
 class MusicServer extends ScalatraFilter with JsonSupport {
   // JsonSupport adds the ability to return JSON objects
@@ -19,6 +22,77 @@ class MusicServer extends ScalatraFilter with JsonSupport {
   
   implicit val formats = Serialization.formats(NoTypeHints)
   
+  // Tags
+  // ====
+  
+  get("/tags") {
+    transaction {
+      val tagsQuery = from(Database.tags)(t => select(t.name))
+      val tags = tagsQuery.toList
+      write(tags)
+    }
+  }
+  
+  put("/tag/:name") {
+    val name = params("name")
+    transaction {
+      Database.tagByName(name) match {
+        case Some(_) => { /* It's already there */ }
+        case None    => {
+          val newTag = new Tag(name)
+          Database.tags.insert(newTag)
+        }
+      }
+    }
+    halt(200)
+  }
+  
+  delete("/tag/:name") {
+    val name = params("name")
+    transaction {
+      Database.tagByName(name) match {
+        case None      => { /* There is no tag like that */ }
+        case Some(tag) => Database.tags.deleteWhere(t => t.id === tag.id)
+      }
+    }
+    halt(200)
+  }
+  
+  put("/tag/:name/:song-id") {
+    val name = params("name")
+    val songId = Long.parseLong(params("song-id"))
+    transaction {
+      Database.tagByName(name).map(tag =>
+        Database.songs.lookup(songId).map(song =>
+          Database.songTags.lookup(compositeKey(song.id, tag.id)) match {
+            case Some(_) => { /* Already exists */ }
+            case None    => {
+              val songTag = new SongTag(song.id, tag.id)
+              Database.songTags.insert(songTag)
+            }
+          }
+        )
+      )
+    }
+    halt(200)
+  }
+  
+  delete("/tag/:name/:song-id") {
+    val name = params("name")
+    val songId = Long.parseLong(params("song-id"))
+    transaction {
+      Database.tagByName(name).map(tag =>
+        Database.songs.lookup(songId).map(song =>
+          Database.songTags.lookup(compositeKey(song.id, tag.id)) match {
+            case None     => { /* Already deleted */ }
+            case Some(st) => Database.songTags.deleteWhere(x =>
+              x.songId === st.songId and x.tagId === st.tagId)
+          }
+        )
+      )
+    }
+    halt(200)
+  }
   
   notFound {  // Executed when no other route succeeds
     JNull
