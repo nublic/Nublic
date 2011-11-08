860ad16abc26e448904f1e1ba7a08868d84f3c27
serras <trupill@gmail.com>
2011-11-08 17:38:06 +0100

    [app.manager.web] Create tabs for favourites [app.manager.server] Fix bug on favourites retrieval

--- apps/app.manager/web/src/main/java/com/nublic/app/manager/web/client/ManagerUi.java
+++ apps/app.manager/web/src/main/java/com/nublic/app/manager/web/client/ManagerUi.java
@@ -5,12 +5,16 @@ import java.util.HashMap;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Document;
 import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.http.client.RequestBuilder;
 import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.Window.Location;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HasVerticalAlignment;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Hyperlink;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.LayoutPanel;
 import com.google.gwt.user.client.ui.TabBar;
@@ -20,6 +24,7 @@ import com.nublic.app.manager.web.frame.AppUrlChangeEvent;
 import com.nublic.app.manager.web.frame.AppUrlChangeHandler;
 import com.nublic.app.manager.web.frame.Counter;
 import com.nublic.app.manager.web.welcome.WelcomePage;
+import com.nublic.util.messages.SequenceHelper;
 
 public class ManagerUi extends Composite implements AppUrlChangeHandler {
 	
@@ -43,12 +48,38 @@ public class ManagerUi extends Composite implements AppUrlChangeHandler {
 	}
 
 	public ManagerUi() {
-		initWidget(uiBinder.createAndBindUi(this));	
-		// Initialize the model
-		// Get apps from server
+		initWidget(uiBinder.createAndBindUi(this));
+		// Initialize client state
 		this.history = new ExtendedHistory(Location.getHref());
 		this.state = ClientState.INITIAL;
 		this.path = "";
+		// Get apps from server
+		AppDataMessage msg = new AppDataMessage(this);
+		SequenceHelper.sendJustOne(msg, RequestBuilder.GET);
+		// Add tab for welcome
+		addTab("welcome", "Home", "images/home.png", "welcome");
+	}
+	
+	void loadApps(HashMap<String, AppData> apps) {
+		this.apps = apps;
+		// Initialize set of favourites
+		for (AppData data : apps.values()) {
+			if (data.isFavourite()) {
+				addTab(data.getId(), data.getDefaultName(),
+						GWT.getHostPageBaseURL() + "manager/server/app-image/" + data.getId() + "/32",
+						data.getId() + "/" + data.getPath());
+			}
+		}
+	}
+	
+	void addTab(String id, String name, String imageUrl, String token) {
+		HorizontalPanel panel = new HorizontalPanel();
+		panel.setVerticalAlignment(HasVerticalAlignment.ALIGN_MIDDLE);
+		Image imageLabel = new Image(imageUrl);
+		panel.add(imageLabel);
+		Hyperlink nameLabel = new Hyperlink(name, token);
+		panel.add(nameLabel);
+		appBar.addTab(panel);
 	}
 	
 	void setContentWidget(Widget w) {
