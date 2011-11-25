dadfa9c525b490ecde4e4d387f6655d2c621e766
eli <eli@nublic.com>
2011-11-25 12:58:47 +0100

    [app.music.web] First approach to the Music App interface. Still a lot to do.

---
+++ apps/app.music/web/src/main/java/com/nublic/app/music/client/ArtListWidget.java
@@ -0,0 +1,41 @@
+package com.nublic.app.music.client;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.cellview.client.CellList;
+import com.google.gwt.view.client.SelectionChangeEvent;
+import com.nublic.app.music.client.ArtistCell.Images;
+
+public class ArtListWidget extends Composite {
+
+	private static ArtListWidgetUiBinder uiBinder = GWT
+			.create(ArtListWidgetUiBinder.class);
+	@UiField(provided=true) CellList<Artist> cellList = null;
+
+	interface ArtListWidgetUiBinder extends UiBinder<Widget, ArtListWidget> {
+	}
+
+	public ArtListWidget() {
+		Images images = GWT.create(Images.class);  
+		 // Create a CellList.
+		ArtistCell artcell = new ArtistCell(images.contact());
+		cellList = new CellList<Artist>(artcell);
+		
+		// Set the total row count. You might send an RPC request to determine the
+	    // total row count.
+	    cellList.setRowCount(4, true);	    
+	    // Set the range to display. In this case, our visible range is smaller than
+	    // the data set.
+	    cellList.setVisibleRange(0, 3);
+   
+	    DataProvider dataProvider = new DataProvider(); 
+
+	    // Connect the list to the data provider.
+	    dataProvider.addDataDisplay(cellList);	    
+		initWidget(uiBinder.createAndBindUi(this));
+	}
+}
