a9f5feadc8394185c4d18a29fbe900ba70a14ae7
PabloTunnon <pablo@nublic.com>
2011-12-10 20:19:33 +0100

    [app.browser.web] Neither to delete not writable files

--- apps/app.browser/web/src/main/java/com/nublic/app/browser/web/client/UI/actions/DeleteAction.java
+++ apps/app.browser/web/src/main/java/com/nublic/app/browser/web/client/UI/actions/DeleteAction.java
@@ -2,6 +2,8 @@ package com.nublic.app.browser.web.client.UI.actions;
 
 import java.util.Set;
 
+import com.google.common.base.Predicate;
+import com.google.common.collect.Sets;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.http.client.RequestBuilder;
 import com.google.gwt.http.client.Response;
@@ -43,15 +45,28 @@ public class DeleteAction extends ActionWidget {
 
 	@Override
 	public Availability getAvailability() {
-		// TODO: not permit delete depending on the place.. ? (ex: don't allow to delete a whole computer)
 		Set<Widget> selected = stateProvider.getSelectedFiles();
-		if (selected.isEmpty()) {
-			setExtraInfo(null);
-			return Availability.UNCLICKABLE;
+		
+		// Check if any of the selected files is not writable (we won't allow cut in that case)
+		// TODO: isn't there a better way to check what is writable?
+		Set<Widget> writables = Sets.filter(selected, new Predicate<Widget>() {
+			@Override
+			public boolean apply(Widget input) {
+				return ((FileWidget) input).isWritable();
+			}
+		});
+
+		if (writables.size() != selected.size()) {
+			return Availability.HIDDEN;
 		} else {
-			// To give feedback on the number of selected files to delete
-			setExtraInfo(String.valueOf(selected.size()));
-			return Availability.AVAILABLE;
+			if (selected.isEmpty()) {
+				setExtraInfo(null);
+				return Availability.UNCLICKABLE;
+			} else {
+				// To give feedback on the number of selected files to delete
+				setExtraInfo(String.valueOf(selected.size()));
+				return Availability.AVAILABLE;
+			}
 		}
 	}
 
