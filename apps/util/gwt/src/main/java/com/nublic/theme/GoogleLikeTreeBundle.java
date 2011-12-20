6bcea41026b95a1fcb3cd3cd1b7bc91ea0e4f647
serras <trupill@gmail.com>
2011-12-20 14:49:20 +0100

    [theming] Changes for better tree views [util.gwt] Added resources for new Google-like tree view

---
+++ apps/util/gwt/src/main/java/com/nublic/theme/GoogleLikeTreeBundle.java
@@ -0,0 +1,11 @@
+package com.nublic.theme;
+
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.ImageResource;
+
+public interface GoogleLikeTreeBundle extends ClientBundle {
+	@Source("arrow_right.png")
+	public ImageResource arrowRight();
+	@Source("arrow_down.png")
+	public ImageResource arrowDown();
+}
