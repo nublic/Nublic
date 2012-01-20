3afa4c977d7d5c42fed17f1b05e3ee415f94564d
serras <trupill@gmail.com>
2012-01-20 11:37:32 +0100

    [app.browser.web] Make polling work correctly

--- apps/app.browser/server/src/main/scala/com/nublic/app/browser/server/filewatcher/DeletionProcessor.scala
+++ apps/app.browser/server/src/main/scala/com/nublic/app/browser/server/filewatcher/DeletionProcessor.scala
@@ -1,30 +1,70 @@
 package com.nublic.app.browser.server.filewatcher
 
 import com.nublic.filewatcher.scala._
+import scala.collection.JavaConversions._
+import java.util.Date
+import java.util.ArrayList
 
 class DeletionProcessor(watcher: FileWatcherActor) extends Processor("deletion", watcher) {
 
-  def TIME_TO_WAIT = 1 /* min */ * 60 /* sec */ * 1000 /* ms */
+  def TIME_TO_WAIT = 5 /* min */ * 60 /* sec */ * 1000 /* ms */
   
-  var deleted_files_list = List[Tuple2[String, Long]]()
+  var new_files_list     = new ArrayList[Tuple2[String, Long]]()
+  var deleted_files_list = new ArrayList[Tuple2[String, Long]]()
   
-  def getDeletedFilesSince(path : String, moment : Long) : List[String] = 
-    deleted_files_list.filter(f => f._1.startsWith(path + "/") && f._2 > moment).map(_._1)
+  private def get_files_since(lst: ArrayList[Tuple2[String, Long]], path: String, moment: Long) : List[String] =
+    lst.filter(f => is_in_folder(f._1, path) && f._2 > moment).map(_._1).toList
   
-  def process(c: FileChange): Unit = c match {
-    case Deleted(filename, _, _)  => process_deletion(filename)
-    case _                        => { /* Do nothing */ }
+  private def is_in_folder(filename: String, folder: String) = {
+    if (filename.startsWith(folder + "/")) {
+      val just_file = filename.substring(folder.length() + 1)
+      !just_file.contains('/')
+    } else {
+      false;
+    }
   }
+  
+  def getNewFilesSince(path: String, moment: Long) : List[String] =
+    get_files_since(new_files_list, path, moment)
+    
+  def getDeletedFilesSince(path: String, moment: Long) : List[String] = 
+    get_files_since(deleted_files_list, path, moment)
+  
+  def process(c: FileChange): Unit =  {
+    val now = (new Date()).getTime()
+    c match {
+      case Created(filename, _, _)  => process_new(filename, now)
+      case Modified(filename, _, _) => process_new(filename, now)
+      case Moved(from, to, _, _)    => {
+        process_deleted(from, now)
+        process_new(to, now)
+      }
+      case Deleted(filename, _, _)  => process_deleted(filename, now)
+      case _                        => { /* Do nothing */ }
+    }
+  }
+  
+  def process_new(filename: String, now: Long) = process_in(new_files_list, deleted_files_list, filename, now)
+  
+  def process_deleted(filename: String, now: Long) = process_in(deleted_files_list, new_files_list, filename, now)
 
-  def process_deletion(filename: String) = {
-    val now = System.currentTimeMillis()
-    deleted_files_list ::= Tuple2(filename, now)
-    delete_old_deleted_files()
+  def process_in(lst: ArrayList[Tuple2[String, Long]], other: ArrayList[Tuple2[String, Long]], filename: String, now: Long) = {
+    val t = Tuple2(filename, now)
+    // Delete previous events of the same path
+    val to_remove = lst.filter(f => f._1 == filename).toList
+    lst.removeAll(to_remove)
+    // Add the current event
+    lst.add(t)
+    // Remove from the other list (if it was created and later removed, we only tell about removing)
+    val to_remove_from_other = other.filter(f => f._1 == filename).toList
+    other.remove(t)
+    // Remove very old notifications
+    delete_old(lst, now)
   }
   
-  def delete_old_deleted_files() = {
-    val now = System.currentTimeMillis()
+  def delete_old(lst: ArrayList[Tuple2[String, Long]], now: Long) = {
     val threshold = now - TIME_TO_WAIT
-    deleted_files_list = deleted_files_list.filter(f => f._2 < threshold)
+    val to_remove = lst.filter(f => f._2 < threshold).toList
+    lst.removeAll(to_remove)
   }
 }
