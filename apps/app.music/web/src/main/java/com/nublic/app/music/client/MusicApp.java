c10a2c18ead86cc38907167793615fd07d229593
PabloTunnon <pablo@nublic.com>
2012-01-24 18:36:41 +0100

    [app.music.web] Reseted project

--- apps/app.music/web/src/main/java/com/nublic/app/music/client/MusicApp.java
+++ apps/app.music/web/src/main/java/com/nublic/app/music/client/MusicApp.java
@@ -1,41 +1,16 @@
 package com.nublic.app.music.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.logical.shared.ValueChangeEvent;
-import com.google.gwt.event.logical.shared.ValueChangeHandler;
-import com.google.gwt.user.client.History;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.RootLayoutPanel;
-import com.nublic.app.music.client.ArtistCell.Images;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
-public class MusicApp implements EntryPoint, ValueChangeHandler<String> {
-
-	MusicUi theUi;
+public class MusicApp implements EntryPoint {
 
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		
-		theUi = new MusicUi();
-		RootLayoutPanel rp = RootLayoutPanel.get();
-	    rp.add(theUi);
-	    
-	    //analizar 
-	    String startingToken = History.getToken();
-	    History.newItem(startingToken);
-	    History.addValueChangeHandler(this);
-	    History.fireCurrentHistoryState();
 	}
 
-	@Override
-	public void onValueChange(ValueChangeEvent<String> event) {
-		// TODO Analizar
-		String url = event.getValue();
-		Window.alert(url);
-	}
 }
