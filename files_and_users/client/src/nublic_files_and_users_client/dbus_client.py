19bf03163f7889bee6aaf467869e7c806653078c
serras <trupill@gmail.com>
2012-02-04 11:31:48 +0100

    [app.manager.server] Make PUT and DELETE methods work [files_and_users.client] Add renaming functionality to nublic-user-manage

--- files_and_users/client/src/nublic_files_and_users_client/dbus_client.py
+++ files_and_users/client/src/nublic_files_and_users_client/dbus_client.py
@@ -11,31 +11,37 @@ def create_user(username, name, password):
     dbus_loop = DBusGMainLoop()
     bus = dbus.SystemBus(mainloop = dbus_loop)
     userService = bus.get_object('com.nublic.users', '/com/nublic/Users')
-    userService.create_user(username, password, name, dbus_interface= 'com.nublic.users')
+    userService.create_user(username, password, name, dbus_interface='com.nublic.users')
+
+def change_user_name(username, name):
+    dbus_loop = DBusGMainLoop()
+    bus = dbus.SystemBus(mainloop = dbus_loop)
+    userService = bus.get_object('com.nublic.users', '/com/nublic/Users')
+    userService.change_user_shown_name(username, name, dbus_interface='com.nublic.users')
 
 def delete_user(username):
     dbus_loop = DBusGMainLoop()
     bus = dbus.SystemBus(mainloop = dbus_loop)
     userService = bus.get_object('com.nublic.users', '/com/nublic/Users')
-    userService.delete_user(username, dbus_interface= 'com.nublic.users')
+    userService.delete_user(username, dbus_interface='com.nublic.users')
 
 def create_mirror(name, owner):
     dbus_loop = DBusGMainLoop()
     bus = dbus.SystemBus(mainloop = dbus_loop)
     mirrorService = bus.get_object('com.nublic.files', '/com/nublic/Mirrors')
-    return mirrorService.create_mirror(name, owner, dbus_interface= 'com.nublic.files')
+    return mirrorService.create_mirror(name, owner, dbus_interface='com.nublic.files')
 
 def delete_mirror(mid):
     dbus_loop = DBusGMainLoop()
     bus = dbus.SystemBus(mainloop = dbus_loop)
     mirrorService = bus.get_object('com.nublic.files', '/com/nublic/Mirrors')
-    mirrorService.delete_mirror(mid, dbus_interface= 'com.nublic.files')
+    mirrorService.delete_mirror(mid, dbus_interface='com.nublic.files')
 
 def list_mirrors():
     dbus_loop = DBusGMainLoop()
     bus = dbus.SystemBus(mainloop = dbus_loop)
     mirrorService = bus.get_object('com.nublic.files', '/com/nublic/Mirrors')
-    notSplitted = mirrorService.get_all_mirrors(dbus_interface= 'com.nublic.files')
+    notSplitted = mirrorService.get_all_mirrors(dbus_interface='com.nublic.files')
     if notSplitted == '':
         return []
     # Take elements apart
@@ -43,8 +49,8 @@ def list_mirrors():
     mirrorList = []
     for midS in splitted:
         mid = int(midS)
-        name = mirrorService.get_mirror_name(mid, dbus_interface= 'com.nublic.files')
-        owner = mirrorService.get_mirror_owner(mid, dbus_interface= 'com.nublic.files')
+        name = mirrorService.get_mirror_name(mid, dbus_interface='com.nublic.files')
+        owner = mirrorService.get_mirror_owner(mid, dbus_interface='com.nublic.files')
         mirrorList.append({ 'id': mid, 'name': name, 'owner': owner })
     return mirrorList
 
@@ -52,19 +58,19 @@ def create_synced_folder(name, owner):
     dbus_loop = DBusGMainLoop()
     bus = dbus.SystemBus(mainloop = dbus_loop)
     mirrorService = bus.get_object('com.nublic.files', '/com/nublic/SyncedFolders')
-    return mirrorService.create_synced_folder(name, owner, dbus_interface= 'com.nublic.files')
+    return mirrorService.create_synced_folder(name, owner, dbus_interface='com.nublic.files')
 
 def delete_synced_folder(mid):
     dbus_loop = DBusGMainLoop()
     bus = dbus.SystemBus(mainloop = dbus_loop)
     mirrorService = bus.get_object('com.nublic.files', '/com/nublic/SyncedFolders')
-    mirrorService.delete_synced_folder(mid, dbus_interface= 'com.nublic.files')
+    mirrorService.delete_synced_folder(mid, dbus_interface='com.nublic.files')
 
 def list_synced_folders():
     dbus_loop = DBusGMainLoop()
     bus = dbus.SystemBus(mainloop = dbus_loop)
     mirrorService = bus.get_object('com.nublic.files', '/com/nublic/SyncedFolders')
-    notSplitted = mirrorService.get_all_synced_folders(dbus_interface= 'com.nublic.files')
+    notSplitted = mirrorService.get_all_synced_folders(dbus_interface='com.nublic.files')
     if notSplitted == '':
         return []
     # Take elements apart
@@ -72,7 +78,7 @@ def list_synced_folders():
     mirrorList = []
     for midS in splitted:
         mid = int(midS)
-        name = mirrorService.get_synced_folder_name(mid, dbus_interface= 'com.nublic.files')
-        owner = mirrorService.get_synced_folder_owner(mid, dbus_interface= 'com.nublic.files')
+        name = mirrorService.get_synced_folder_name(mid, dbus_interface='com.nublic.files')
+        owner = mirrorService.get_synced_folder_owner(mid, dbus_interface='com.nublic.files')
         mirrorList.append({ 'id': mid, 'name': name, 'owner': owner })
     return mirrorList
