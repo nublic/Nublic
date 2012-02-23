9ace0a581a4b81206e659421010cce699b123434
serras <trupill@gmail.com>
2012-02-23 16:37:01 +0100

    [app.browser.web] Lazy load ACE web editor

--- apps/app.browser/web/src/main/java/com/nublic/app/browser/web/client/UI/BrowserUi.java
+++ apps/app.browser/web/src/main/java/com/nublic/app/browser/web/client/UI/BrowserUi.java
@@ -17,6 +17,7 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.RunAsyncCallback;
+import com.google.gwt.core.client.ScriptInjector;
 import com.google.gwt.event.dom.client.ChangeEvent;
 import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -710,6 +711,32 @@ public class BrowserUi extends Composite implements ModelUpdateHandler, OpenHand
 	}
 	
 	public void showText(final String path) {
+		ScriptInjector.fromUrl("ace/ace.js").setCallback(new com.google.gwt.core.client.Callback<Void, Exception>() {
+			
+			@Override
+			public void onSuccess(Void result) {
+				ScriptInjector.fromUrl("ace/theme-eclipse.js").setCallback(new com.google.gwt.core.client.Callback<Void, Exception>() {
+					
+					@Override
+					public void onSuccess(Void result) {
+						showTextInternal(path);
+					}
+					
+					@Override
+					public void onFailure(Exception reason) {
+						Window.alert("Error loading text viewer");	
+					}
+				}).inject();
+			}
+			
+			@Override
+			public void onFailure(Exception reason) {
+				Window.alert("Error loading text viewer");
+			}
+		}).inject();
+	}
+	
+	private void showTextInternal(final String path) {
 		GWT.runAsync(new RunAsyncCallback() {
 			
 			@Override
