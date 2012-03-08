543b1c85e1bf9f4f474e12826f401aa0662153a1
serras <trupill@gmail.com>
2012-03-08 15:42:33 +0100

    [app.music.web] Change thumbnail size to 48x48

--- apps/app.music/server/src/main/scala/com/nublic/app/music/server/MusicFolder.scala
+++ apps/app.music/server/src/main/scala/com/nublic/app/music/server/MusicFolder.scala
@@ -8,7 +8,7 @@ object MusicFolder {
   val ALBUMS_FOLDER = ROOT_FOLDER + "/albums"
   val ORIGINAL_FILENAME = "orig"
   val THUMBNAIL_FILENAME = "thumb.png"
-  val ARTIST_THUMBNAIL_SIZE = 96
+  val ARTIST_THUMBNAIL_SIZE = 48
   
   def getArtistFolder(id: Long): File = new File(ARTISTS_FOLDER, id.toString())
   def getAlbumFolder(id: Long): File = new File(ALBUMS_FOLDER, id.toString())
