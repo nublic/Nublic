9af99faa9bf78f24701d02c5149fe64920a01889
serras <trupill@gmail.com>
2012-03-29 12:18:05 +0200

    [app.photos.web] Create Photos app skeleton

---
+++ apps/app.photos/web/src/main/java/com/nublic/app/photos/web/client/PhotosApp.java
@@ -0,0 +1,27 @@
+package com.nublic.app.photos.web.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class PhotosApp implements EntryPoint {
+
+	// WelcomePage theUi;
+	
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+		initUi();
+		Window.enableScrolling(false);
+	}
+	
+	public void initUi() {
+		// theUi = new WelcomePage();
+		RootLayoutPanel rp = RootLayoutPanel.get();
+	    // rp.add(theUi);
+	}	
+}
