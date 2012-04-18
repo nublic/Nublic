19202fd53ed278176d3460dca5580a3875c622e6
PabloTunnon <pablo@nublic.com>
2012-04-18 11:08:14 +0200

    [app.music.web] Changed album proxy

--- apps/app.music/web/src/main/java/com/nublic/app/music/client/ui/dnd/proxy/AlbumDragProxy.java
+++ apps/app.music/web/src/main/java/com/nublic/app/music/client/ui/dnd/proxy/AlbumDragProxy.java
@@ -1,44 +1,58 @@
 package com.nublic.app.music.client.ui.dnd.proxy;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ErrorEvent;
+import com.google.gwt.event.dom.client.ErrorHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
+import com.nublic.app.music.client.Resources;
+import com.nublic.app.music.client.datamodel.AlbumInfo;
+import com.nublic.app.music.client.datamodel.Controller;
+import com.nublic.util.cache.CacheHandler;
 
 public class AlbumDragProxy extends Composite implements DragProxy {
 	private static AlbumDragProxyUiBinder uiBinder = GWT.create(AlbumDragProxyUiBinder.class);
 	interface AlbumDragProxyUiBinder extends UiBinder<Widget, AlbumDragProxy> { }
 	
-	@UiField Label text;
+	@UiField Label title;
+	@UiField Label numberOfSongs;
 	@UiField SimplePanel plusPanel;
-	
-//	public SongDragProxy(final SongInfo draggingSong) {
-//		initWidget(uiBinder.createAndBindUi(this));
-//		
-//		Controller.INSTANCE.getModel().getArtistCache().addHandler(draggingSong.getArtistId(), new CacheHandler<String, ArtistInfo>() {
-//			@Override
-//			public void onCacheUpdated(String k, ArtistInfo v) {
-//				text.setText(v.getName() + " - " + draggingSong.getTitle());
-//			}
-//		});
-//		Controller.INSTANCE.getModel().getArtistCache().obtain(draggingSong.getArtistId());
-//		
-//
-//		setState(ProxyState.NONE);
-//	}
+	@UiField Image albumArt;
+
 	public AlbumDragProxy(String draggingAlbumId) {
 		initWidget(uiBinder.createAndBindUi(this));
 		
-		setText(draggingAlbumId);
+		Controller.INSTANCE.getModel().getAlbumCache().addHandler(draggingAlbumId, new CacheHandler<String, AlbumInfo>() {
+			@Override
+			public void onCacheUpdated(String k, AlbumInfo v) {
+				numberOfSongs.setText(v.getNumberOfSongs() + " songs");
+				title.setText(v.getName());
+				setImage(v);
+			}
+		});
+		Controller.INSTANCE.getModel().getAlbumCache().obtain(draggingAlbumId);
+
 		setState(ProxyState.NONE);
+	}
+	
+	private void setImage(AlbumInfo album) {
+		albumArt.addErrorHandler(new ErrorHandler() {
+			@Override
+			public void onError(ErrorEvent event) {
+				albumArt.setResource(Resources.INSTANCE.album());
+			}
+		});
+		albumArt.setUrl(album.getImageUrl());
 	}
 
 	
 	public void setText(String text) {
-		this.text.setText(text);
+		this.title.setText(text);
 	}
 
 	@Override
