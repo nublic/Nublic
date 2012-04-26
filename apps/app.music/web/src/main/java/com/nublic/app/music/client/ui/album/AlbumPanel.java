b71641c3cc52f74c5f5356d48ea0751256ab7bd8
PabloTunnon <pablo@nublic.com>
2012-04-26 18:48:54 +0200

    [app.music.web] Trying to use tab style...

--- apps/app.music/web/src/main/java/com/nublic/app/music/client/ui/album/AlbumPanel.java
+++ apps/app.music/web/src/main/java/com/nublic/app/music/client/ui/album/AlbumPanel.java
@@ -6,6 +6,7 @@ import java.util.List;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
@@ -33,6 +34,7 @@ public class AlbumPanel extends Composite {
 	@UiField Label titleLabel;
 	@UiField HorizontalPanel titlePanel;
 	@UiField InlineHyperlink artistViewLink;
+	@UiField InlineHyperlink albumViewLink;
 	@UiField InlineHyperlink songViewLink;
 	@UiField PushButton backButton;
 	
@@ -68,7 +70,9 @@ public class AlbumPanel extends Composite {
 			String collectionName = Controller.INSTANCE.getModel().getTagCache().get(collectionId).getName();
 			titleLabel.setText(titleLabel.getText() + " - " + collectionName);
 		}
-
+		
+		unableAlbumLink();
+		
 		// Create button line
 		EnumSet<ButtonLineParam> buttonSet = EnumSet.of(ButtonLineParam.ADD_AT_END,
 														ButtonLineParam.PLAY);
@@ -76,6 +80,10 @@ public class AlbumPanel extends Composite {
 		setAddAtEndButtonHandler(b);
 		setPlayButtonHandler(b);
 		titlePanel.insert(b, 2);
+	}
+
+	private void unableAlbumLink() {
+		albumViewLink.setTargetHistoryToken(History.getToken());
 	}
 
 	public void setAlbumList(List<AlbumInfo> albumList) {
