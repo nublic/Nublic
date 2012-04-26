483a3039d965934e6159788105d62750a1580da2
PabloTunnon <pablo@nublic.com>
2012-04-26 19:54:09 +0200

    [app.music.web] Internationaliting... (prepare yourself for a 12 permutations build...)

--- apps/app.music/web/src/main/java/com/nublic/app/music/client/Constants.java
+++ apps/app.music/web/src/main/java/com/nublic/app/music/client/Constants.java
@@ -1,6 +1,11 @@
 package com.nublic.app.music.client;
 
+import com.google.gwt.core.client.GWT;
+import com.nublic.app.music.client.i18n.MyConstants;
+
 public class Constants {
+	public static MyConstants I18NCONSTANTS = GWT.create(MyConstants.class);
+	
 	public static final String PARAM_COLLECTION = "Collection";
 	public static final String PARAM_PLAYLIST = "Playlist";
 	public static final String PARAM_ARTIST = "Artist";
@@ -17,8 +22,9 @@ public class Constants {
 	public static final String CURRENT_PLAYLIST_ID = "CurrentPlaylist";
 	public static final String CURRENT_PLAYLIST_NAME = "Current playlist";
 	public static final String ALL_MUSIC_NAME = "All music";
-	public static final String CONFIRM_DELETION_TITLE = "Confirm deletion?";
-	public static final String CONFIRM_DELETION_INFO = "This action will erase the collection, but not any of the song files. You will still be able to find them in \"All music\" and/or in other collections";
+
+//	public static final String CONFIRM_DELETION_TITLE = "Confirm deletion?";
+//	public static final String CONFIRM_DELETION_INFO = "This action will erase the collection, but not any of the song files. You will still be able to find them in \"All music\" and/or in other collections";
 	
 	public static final int PREVIOUS_SONGS_TO_ASK = 10;
 	public static final int NEXT_SONGS_TO_ASK = 24;
---
+++ apps/app.music/web/src/main/java/com/nublic/app/music/client/i18n/MyConstants.java
@@ -0,0 +1,8 @@
+package com.nublic.app.music.client.i18n;
+
+import com.google.gwt.i18n.client.Constants;
+
+public interface MyConstants extends Constants {
+	String confirmDeletionTitle();
+	String confirmDeletionInfo();
+}
