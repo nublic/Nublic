558ba9b0e75babfe2c42f416aa96dbb58d5f5316
PabloTunnon <pablo@nublic.com>
2012-05-10 11:18:56 +0200

    [util.gwt] I18N

---
+++ apps/util/gwt/src/main/java/com/nublic/util/i18n/I18NMessages.java
@@ -0,0 +1,17 @@
+package com.nublic.util.i18n;
+
+import com.google.gwt.i18n.client.Messages;
+import com.google.gwt.i18n.client.LocalizableResource.DefaultLocale;
+
+@DefaultLocale("en")
+public interface I18NMessages extends Messages {
+	// Widgets
+	String cancel();
+	String no();
+	String custom();
+	String add();
+	String delete();
+	String upload();
+	String yes();
+	String ok();
+}
