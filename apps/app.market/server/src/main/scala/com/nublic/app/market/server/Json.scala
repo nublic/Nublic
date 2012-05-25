a7a9ecbd7e2fe7101d948e499e1301a442ce0fb3
serras <trupill@gmail.com>
2012-05-25 13:09:13 +0200

    [app.market] Change json schema

--- apps/app.market/server/src/main/scala/com/nublic/app/market/server/Json.scala
+++ apps/app.market/server/src/main/scala/com/nublic/app/market/server/Json.scala
@@ -2,10 +2,12 @@ package com.nublic.app.market.server
 
 case class TranslatedText(val default: String, val translated: Map[String, String])
 
+case class Url(val text: TranslatedText, val url: String)
+
 case class Package(val id: String, val icon: String, val name: TranslatedText,
                    val short_description: TranslatedText, val long_description: TranslatedText,
                    val screenshots: List[String], val categories: List[String],
-                   val links: List[String], val developer: String,
+                   val links: List[Url], val developer: Url,
                    val deb: String, val status:Option[String])
 
 object Package {
