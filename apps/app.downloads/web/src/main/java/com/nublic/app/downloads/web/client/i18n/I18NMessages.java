aca07a4c42a7f171ac42e532c0b124354b49eab9
Alejandro Serrano <trupill@gmail.com>
2012-06-06 17:21:00 +0200

    [app.downloads.web] Initial app skeleton

---
+++ apps/app.downloads/web/src/main/java/com/nublic/app/downloads/web/client/i18n/I18NMessages.java
@@ -0,0 +1,10 @@
+package com.nublic.app.downloads.web.client.i18n;
+
+import com.google.gwt.i18n.client.Messages;
+import com.google.gwt.i18n.client.LocalizableResource.DefaultLocale;
+import com.google.gwt.safehtml.shared.SafeHtml;
+
+@DefaultLocale("en")
+public interface I18NMessages extends Messages {
+	String title();
+}
---
+++ apps/app.downloads/web/src/test/java/com/nublic/app/downloads/web/client/DownloadsAppTest.java
@@ -0,0 +1,21 @@
+package com.nublic.app.downloads.web.client;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+/**
+ * GWT JUnit tests must extend GWTTestCase.
+ */
+public class DownloadsAppTest extends GWTTestCase {
+
+  /**
+   * Must refer to a valid module that sources this class.
+   */
+  public String getModuleName() {
+    return "com.nublic.app.downloads.web.DownloadsAppJUnit";
+  }
+
+  public void testNothing() {
+    assertTrue(true);
+  }
+
+}
