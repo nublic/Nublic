ec94b0a1f943ebeeb85cf7381a0ee3445bfa74f2
Alejandro Serrano <trupill@gmail.com>
2012-08-13 17:39:22 +0200

    [app.music.server] Port extractors

--- apps/app.music/server-python/src/nublic_app_music_server/song_info.py
+++ apps/app.music/server-python/src/nublic_app_music_server/song_info.py
@@ -1,6 +1,11 @@
 import mad
 import mutagen
+import os.path
 import re
+import subprocess
+
+from pyechonest import config
+from pyechonest.song import identify
 
 class SongInfo:
     def __init__(self, title, artist, album, length, year, track, disc_no):
@@ -29,15 +34,30 @@ class SongInfo:
         else:
             return self.clone()
 
+def get_song_info(file_, context):
+    tag_info = extract_using_mutagen(file_).clean()
+    if tag_info.has_important_info_missing():
+        echonest_info = extract_using_echonest(file_)
+        tag_info = merge_song_infos(tag_info, echonest_info)
+    if tag_info.has_important_info_missing():
+        file_info = extract_using_filename(file_, context)
+        tag_info = merge_song_infos(tag_info, file_info)
+    return tag_info
+
 def merge_song_infos(s1, s2):
-    title = _merge_info(s1.title, s2.title)
-    artist = _merge_info(s1.artist, s2.artist)
-    album = _merge_info(s1.album, s2.album)
-    length = _merge_info(s1.length, s2.length)
-    year = _merge_info(s1.year, s2.year)
-    track = _merge_info(s1.track, s2.track)
-    disc_no = _merge_info(s1.disc_no, s2.disc_no)
-    return SongInfo(title, artist, album, length, year, track, disc_no)
+    if s1 == None:
+        return s2
+    elif s2 == None:
+        return s1
+    else:
+        title = _merge_info(s1.title, s2.title)
+        artist = _merge_info(s1.artist, s2.artist)
+        album = _merge_info(s1.album, s2.album)
+        length = _merge_info(s1.length, s2.length)
+        year = _merge_info(s1.year, s2.year)
+        track = _merge_info(s1.track, s2.track)
+        disc_no = _merge_info(s1.disc_no, s2.disc_no)
+        return SongInfo(title, artist, album, length, year, track, disc_no)
 
 def _merge_info(i1, i2):
     if i1 != None and i1 != '':
@@ -49,12 +69,17 @@ def _merge_info(i1, i2):
 # ==========
 
 def empty_song_info():
-    return SongInfo('', '', '', '', '', '', '')
+    return SongInfo('', '', '', None, None, None, None)
 
 def extract_using_mutagen(file_):
+    # Use Mad for length
     try:
         madf = mad.MadFile(file_)
         length = madf.total_time() // 1000
+    except:
+        length = None
+    # Use Mutagen for tags
+    try:
         mutf = mutagen.File(file_, easy=True)
         if 'title' in mutf:
             title = mutf['title']
@@ -85,5 +110,69 @@ def extract_using_mutagen(file_):
         return empty_song_info()
 
 def extract_using_echonest(file_):
-    pass
-        
+    # Get length
+    try:
+        madf = mad.MadFile(file_)
+        length = madf.total_time() // 1000
+    except:
+        length = None
+    # Identify via Echo Nest
+    config.ECHO_NEST_API_KEY = 'UR4VKX7JXDXAULIWB'
+    config.CODEGEN_BINARY_OVERRIDE = '/usr/bin/echoprint-codegen'
+    possible_songs = identify(filename=file_)
+    if possible_songs:
+        song = possible_songs[0]
+        return SongInfo(song.title, song.artist_name, '', length, None, None, None)
+    else:
+        return empty_song_info()
+
+def extract_using_filename(file_, context):
+    # Get length
+    try:
+        madf = mad.MadFile(file_)
+        length = madf.total_time() // 1000
+    except:
+        length = None
+    # Get information from filename
+    no_context = file_.replace(context + '/', '', 1)
+    parent, filename = os.path.split(no_context)
+    # Initialize to empty
+    track = None
+    title = None
+    artist = None
+    album = None
+    # Try to get track number
+    tmatch = re.match(r"(\d+)\.? *(.*)$", filename)
+    if tmatch != None:
+        track = int(tmatch.group(1))
+        filename = tmatch.group(2).strip()
+    # Try to parse rest of string
+    ar_ab_title_match = re.match(r"\s*(.*)-\s*(.*)-\s*(.*)$", filename)
+    ar_title_match = re.match(r"\s*(.*)-\s*(.*)$", filename)
+    if ar_ab_title_match != None:
+        artist = ar_ab_title_match.group(1).strip()
+        album = ar_ab_title_match.group(2).strip()
+        title = ar_ab_title_match.group(3).strip()
+    elif ar_title_match != None:
+        artist = ar_ab_title_match.group(1).strip()
+        title = ar_ab_title_match.group(2).strip()
+    else:
+        title = filename
+    # Get information from path
+    while parent != '':
+        p_parent, p_name = os.path.split(parent)
+        if album == None:
+            ar_ab_match = re.match(r"\s*(.*)-\s*(.*)$", p_name)
+            if ar_ab_match != None:
+                artist = ar_ab_match.group(1).strip()
+                album = ar_ab_match.group(2).strip()
+            else:
+                album = p_name.strip()
+        else:
+            if artist == None:
+                artist = p_name.strip()
+            else:
+                pass
+        parent = p_parent
+    # Put everythung together
+    return SongInfo(title, artist, album, length, None, track, None)
