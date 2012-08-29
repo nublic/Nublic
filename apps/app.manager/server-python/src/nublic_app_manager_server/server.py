50af1c071c46e5498f115a9f2e8a0b50c6e2a0c1
Alejandro Serrano <trupill@gmail.com>
2012-08-29 19:21:01 +0200

    [app.manager.server] Add support for SparkleShare invites

--- apps/app.manager/server-python/src/nublic_app_manager_server/server.py
+++ apps/app.manager/server-python/src/nublic_app_manager_server/server.py
@@ -1,7 +1,10 @@
+import datetime
 from flask import Flask, request, abort, send_file
 import os
 import os.path
+import random
 import simplejson as json
+import string
 
 from nublic.files_and_users import get_all_users, Mirror, WorkFolder, create_mirror, create_work_folder
 from nublic_server.helpers import init_bare_nublic_server, require_user
@@ -107,7 +110,7 @@ def mirrors():
     elif request.method == 'DELETE':
         mid = int(request.form.get('id'))
         m = Mirror(mid)
-        if m.exists() and m.get_owner() == user.get_username():
+        if m.exists() and m.get_owner().get_username() == user.get_username():
             m.delete(False)
             abort(200)
         else:
@@ -119,7 +122,7 @@ def mirror_name():
     name = request.form.get('name', None)
     mid = int(request.form.get('id'))
     m = Mirror(mid)
-    if name != None and m.exists() and m.get_owner() == user.get_username():
+    if name != None and m.exists() and m.get_owner().get_username() == user.get_username():
         # Change name
         m.change_name(name)
         abort(200)
@@ -149,7 +152,7 @@ def synceds():
     elif request.method == 'DELETE':
         sid = int(request.form.get('id'))
         s = WorkFolder(sid)
-        if s.exists() and s.get_owner() == user.get_username():
+        if s.exists() and s.get_owner().get_username() == user.get_username():
             s.delete(False)
             abort(200)
         else:
@@ -161,10 +164,74 @@ def synced_name():
     name = request.form.get('name', None)
     sid = int(request.form.get('id'))
     s = WorkFolder(sid)
-    if name != None and s.exists() and s.get_owner() == user.get_username():
+    if name != None and s.exists() and s.get_owner().get_username() == user.get_username():
         # Change name
         s.change_name(name)
         abort(200)
+    else:
+        abort(403)
+
+# Work on SSH keys
+current_upload_keys = []
+
+def prune_old_upload_keys():
+    global current_upload_keys
+    five_minutes = datetime.timedelta(minutes=5)
+    five_minutes_before_now = datetime.datetime.now() - five_minutes
+    current_upload_keys = filter(lambda x: x['time'] > five_minutes_before_now, current_upload_keys)
+
+@app.route('/synced-generate-invite/<int:sid>')
+def synced_generate_invite(sid):
+    user = require_user()
+    s = WorkFolder(sid)
+    #app.logger.error('User: %s, existance: %s, owner: %s', user.get_username(), str(s.exists()), s.get_owner().get_username())
+    if s.exists() and s.get_owner().get_username() == user.get_username():
+        random_id = random.randint(100, 2000000)
+        current_upload_keys.append({ 'id': random_id, 'time': datetime.datetime.now(), 'synced_id': sid, 'user': user })
+        return str(random_id)
+    else:
+        abort(403)
+
+@app.route('/synced-invite/<int:iid>')
+def synced_invite(iid):
+    prune_old_upload_keys()
+    invite = [i for i in current_upload_keys if i['id'] == iid]
+    if invite:
+        r_invite = invite[0]
+        s = WorkFolder(r_invite['synced_id'])
+        user = r_invite['user']
+        if s.exists() and s.get_owner().get_username() == user.get_username():
+            manager_path, _ = os.path.split(request.url_root[:-1])
+            server = os.path.split(manager_path)[0].replace('http://', '')
+            xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + \
+                  "<sparkleshare>\n" + \
+                  "  <invite>\n" + \
+                  "    <address>ssh://" + user.get_username() + "@" + server + "/</address>\n" + \
+                  "    <remote_path>/var/nublic/work-folders/" + str(s.get_id()) + "</remote_path>\n" + \
+                  "    <accept_url>http://" + server + "/manager/server/synced-upload-key/" + str(s.get_id()) + "</accept_url>\n" + \
+                  "  </invite>\n" + \
+                  "</sparkleshare>"
+            return xml
+    else:
+        abort(403)
+
+@app.route('/synced-upload-key/<int:iid>')
+def synced_upload_key(iid, methods=['POST']):
+    prune_old_upload_keys()
+    invite = [i for i in current_upload_keys if i['id'] == iid]
+    if invite:
+        # Get user
+        r_invite = invite[0]
+        user = r_invite['user']
+        # Work on public key to make it in correct format
+        pubkey = request.form.get('pubkey', None)
+        token_list = pubkey.split(' ')
+        ssh_initial = token_list[0]
+        ssh_name = token_list[-1]
+        ssh_rest = token_list[1:-1]
+        real_key = ssh_initial + ' ' + string.join(ssh_rest, '+') + ' ' + ssh_name
+        # Finally add key
+        user.add_public_key(real_key)
     else:
         abort(403)
 
