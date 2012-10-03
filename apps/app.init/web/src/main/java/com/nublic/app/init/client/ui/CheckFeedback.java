31ba13ffea006658d2530ff8398e43cf96c67c99
PabloTunnon <pablo@nublic.com>
2012-10-03 16:12:23 +0200

    [app.init.web] Improved CheckFeedback to show messages as well

--- apps/app.init/web/src/main/java/com/nublic/app/init/client/ui/CheckFeedback.java
+++ apps/app.init/web/src/main/java/com/nublic/app/init/client/ui/CheckFeedback.java
@@ -6,6 +6,7 @@ import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.HTMLPanel;
 
 public class CheckFeedback extends Composite {
 	private static CheckFeedbackUiBinder uiBinder = GWT.create(CheckFeedbackUiBinder.class);
@@ -14,6 +15,14 @@ public class CheckFeedback extends Composite {
 	@UiField Image check;
 	@UiField Image cross;
 	@UiField Image loading;
+	@UiField HTMLPanel checkInfo;
+	@UiField HTMLPanel crossInfo;
+	@UiField HTMLPanel loadingInfo;
+	@UiField HTMLPanel noneInfo;
+	String checkText = "";
+	String crossText = "";
+	String loadingText = "";
+	String noneText = "";
 
 	public CheckFeedback() {
 		initWidget(uiBinder.createAndBindUi(this));
@@ -21,10 +30,35 @@ public class CheckFeedback extends Composite {
 		setFeedback(Feedback.NONE);
 	}
 	
+	public void setCheckInfo(String s) {
+		checkText = s;
+		checkInfo.getElement().setInnerText(checkText);
+	}
+	
+	public void setCrossInfo(String s) {
+		crossText = s;
+		crossInfo.getElement().setInnerText(crossText);
+	}
+	
+	public void setLoadingInfo(String s) {
+		loadingText = s;
+		loadingInfo.getElement().setInnerText(loadingText);
+	}
+	
+	public void setNoneInfo(String s) {
+		noneText = s;
+		noneInfo.getElement().setInnerText(noneText);
+	}
+	
 	public void setFeedback(Feedback f) {
 		check.setVisible(f == Feedback.CHECK);
 		cross.setVisible(f == Feedback.CROSS);
 		loading.setVisible(f == Feedback.LOADING);
+		
+		checkInfo.setVisible(f == Feedback.CHECK && !checkText.isEmpty());
+		crossInfo.setVisible(f == Feedback.CROSS && !crossText.isEmpty());
+		loadingInfo.setVisible(f == Feedback.LOADING && !loadingText.isEmpty());
+		noneInfo.setVisible(f == Feedback.NONE && !noneText.isEmpty());
 	}
 
 }
