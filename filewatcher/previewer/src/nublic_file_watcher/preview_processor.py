73aef1bb928104a945037bf02ada1c6a63ca4215
Alejandro Serrano <trupill@gmail.com>
2012-11-14 18:56:54 +0100

    [filewatcher.previewer] Initial steps for previewer

---
+++ filewatcher/previewer/src/nublic_file_watcher/preview_processor.py
@@ -0,0 +1,45 @@
+class Element:
+    def __init__(self, change):
+        self.change = change
+        self.path = change.filename
+        self.info = FileInfo(self.path)
+        self.time = datetime.datetime.now()
+
+    def get_change(self):
+        return self.change
+
+    def get_info(self):
+        return self.info
+
+    def get_time(self):
+        return self.time
+
+    def __le__(self, other):
+        if self.path == other.path:
+            # Same file, we compare the times
+            return self.time <= other.time
+        else:
+            # Different files
+            # First we compare the type of changes
+            # (small time changes are better)
+            if self.is_short_kind() and not other.is_short_kind():
+                return true
+            elif other.is_short_kind() and not self.is_short_kind():
+                return false
+            # Second we compare the size of files
+            return self.info.size() <= other.info.size()
+
+    def is_short_kind(self):
+        return (self.change.kind == FileChange.DELETED or \
+                self.change.kind == FileChange.ATTRIBS_CHANGED or \
+                self.change.kind == FileChange.MOVED)
+
+class PreviewProcessor:
+    def __init__(self):
+        pass
+
+    def process(self, element):
+        raise NotImplementedError("Should be implemented in derived classes")
+
+    def get_id(self):
+        raise NotImplementedError("Should be implemented in derived classes")
--- filewatcher/previewer/src/setup.py
+++ filewatcher/previewer/src/setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='nublic-file-watcher',
-      version='0.2',
+      version='0.3',
       description='The Nublic File Watcher System',
       author='Alejandro Serrano Mena',
       author_email='alex@nublic.com',
