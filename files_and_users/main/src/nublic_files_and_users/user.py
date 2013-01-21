ca578b83f8fdc1435c9d543335ee4addb04e7031
David Navarro <davisein@gmail.com>
2013-01-21 12:54:06 +0100

    Fix references from tomcat7 to jetty user

--- files_and_users/main/src/nublic_files_and_users/user.py
+++ files_and_users/main/src/nublic_files_and_users/user.py
@@ -220,10 +220,10 @@ class UserDBus(): #(dbus.service.Object):
         # Make chown
         real_path = DATA_ROOT + path
         os.chown(real_path, user.uid, self.get_nublic_gid())
-        pexpect.run('setfacl -m u:tomcat7:rwx "' + real_path + '"')
+        pexpect.run('setfacl -m u:jetty:rwx "' + real_path + '"')
         if (touch_after):
             pexpect.run('sudo -u ' + username + ' touch "' + real_path + '"')
-    
+
     #@dbus.service.method('com.nublic.users', in_signature = 'ss', out_signature = '')
     def add_public_key(self, username_, key):
         # Convert to utf-8
