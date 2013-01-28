nublic/Nublic

commit 6b1b13073a9b96fafe32a871505b6eadaeb4e2a1

[resource] Cach exception and includes a bigger pool

resource/main/src/nublic_resource/postgresql_db.py
@@ -119,8 +119,8 @@ class PostgresqlDB(DatabaseStored):
                                     self.__root_user, self.__root_password)
         # Create database cannot be executed as a transaction block so we 
         # should change the isolation level to create a database
-        metadata.bind.engine.connect().\
-            connection.connection.set_isolation_level(0)
+        connection = metadata.bind.engine.connect()
+        conection.connection.connection.set_isolation_level(0)
         
         # Generate data and queries
         database_name, user_name, password = self.__create_random_data()
@@ -143,6 +143,8 @@ class PostgresqlDB(DatabaseStored):
         text(sql_create_user, metadata.bind).execute(user=user_name, password=password)
         text(sql_create_database, metadata.bind).execute(database=database_name)
         #text(sql_revoke_permissions, metadata.bind).execute(user = user_name)
+	connection.close()
+        metadata.bind.engine.dispose()
         # Restores the old database
         metadata.bind = bind
         return database_name, user_name, password
@@ -152,8 +154,8 @@ class PostgresqlDB(DatabaseStored):
         # Creates a connection with permission to create users and databases
         metadata.bind = self.__generate_connection_uri(
                                         self.__root_user, self.__root_password)
-        metadata.bind.engine.connect().\
-            connection.connection.set_isolation_level(0)
+        connection = metadata.bind.engine.connect()
+        conection.connection.connection.set_isolation_level(0)
         
         # Generate data and queries
         sql_delete_database = "DROP DATABASE " + database_name + ";"
@@ -161,6 +163,8 @@ class PostgresqlDB(DatabaseStored):
         # Perform the queries
         text(sql_delete_database, metadata.bind).execute(database=database_name)
         text(sql_delete_user, metadata.bind).execute(user=user_name)
+	connection.close()
+        metadata.bind.engine.dispose()
         # Restores the old database_name
         metadata.bind = bind
 

resource/main/src/nublic_resource/model.py
@@ -11,6 +11,7 @@ from elixir import Entity, Field, using_options
 from elixir import ManyToOne
 from elixir.relationships import OneToMany
 from sqlalchemy.types import Unicode
+from sqlalchemy import create_engine
 import ConfigParser
 
 # Load configuration file for user and password
@@ -19,7 +20,8 @@ __config.read('/etc/nublic/resource.conf')
 postgres_root_password = __config.get('DB_ACCESS','NUBLIC_RESOURCE_PASS').strip("'")
 postgres_root_user = __config.get('DB_ACCESS','NUBLIC_RESOURCE_USER').strip("'")
 
-metadata.bind = "postgresql://"+postgres_root_user+":"+postgres_root_password+'@localhost/nublic_resource'
+
+metadata.bind = create_engine("postgresql://"+postgres_root_user+":"+postgres_root_password+'@localhost/nublic_resource', pool_size = 100)
 metadata.bind.echo = False
 
 class App(Entity):

resource/main/src/nublic_resource/nublic-resource-daemon
@@ -78,8 +78,9 @@ class JsonRpcResource(Handler):
     def value(self, app_name, key, subkey):
         try:
             return get_provider(app_name, key).value(app_name, key, subkey)
-        except:
-            return None 
+        except BaseException as e:
+            logging.error("Detected exception %s", str(e))
+            return None
 
 if __name__ == '__main__':
     if not os.geteuid() == 0:

commit 99d340b1d78855f0e5a8f1d9a891d787a0942f11

Fix a regression introduced in previous commit
The commit 6b1b13073a9b96fafe32a871505b6eadaeb4e2a1 made a cosmetic
change  that makes the connection not to change to the proper isolation
level to be able to create a database.

resource/main/src/nublic_resource/postgresql_db.py
@@ -11,13 +11,13 @@ from random import Random
 from database_stored import DatabaseStored
 from model import metadata
 import model
-from database_stored import ExistingKeyError, \
-    NotExistingKeyError
+from database_stored import (ExistingKeyError,
+                             NotExistingKeyError)
 
 
 class PostgresqlDB(DatabaseStored):
     '''
-    Resource class that provides a database access to postgre.
+    Resource class that provides a database access to postgresql.
     Gives a user with access to a empty database.
 
     Subkeys:
@@ -105,7 +105,7 @@ class PostgresqlDB(DatabaseStored):
 
     def __generate_connection_uri(self, user, password, database=None):
         '''
-        Returns a conection uri with database
+        Returns a connection uri with database
         '''
         if database is None:
             return (self.__connection_protocol + "://" + user + ":" + password
@@ -121,20 +121,24 @@ class PostgresqlDB(DatabaseStored):
             self.__root_user, self.__root_password)
         # Create database cannot be executed as a transaction block so we
         # should change the isolation level to create a database
-        connection = metadata.bind.engine.connect()
-        connection.connection.connection.set_isolation_level(0)
+        metadata.bind.engine.connect().\
+            connection.connection.set_isolation_level(0)
+        #connection = metadata.bind.engine.connect()
+        #conection.connection.connection.set_isolation_level(0)
         # Generate data and queries
         database_name, user_name, password = self.__create_random_data()
         # It is needed to concatenate this way to avoid the usual but
         # incompatible way to escape strings while executing some admin-level
         # sql commands
-        sql_create_user = \
-            "CREATE USER " + user_name + \
-            " WITH PASSWORD '" + password + "';"
-        sql_create_database =  \
-            "CREATE DATABASE " + database_name + \
-            " WITH OWNER " + user_name + \
-            " ENCODING 'UTF8';"  # @TODO: Check if it is correct
+        sql_create_user = (
+            "CREATE USER " + user_name +
+            " WITH PASSWORD '" + password + "';")
+        sql_create_database = (
+            "CREATE DATABASE " + database_name +
+            " WITH OWNER " + user_name +
+            " ENCODING 'UTF8' "  # @TODO: Check if it is correct
+            " LC_CTYPE='en_US.utf8'"
+            " LC_COLLATE='en_US.utf8' TEMPLATE template0;")
         # Show the queries
         print("LOG: " + sql_create_user)
         print("LOG: " + sql_create_database)
@@ -144,7 +148,8 @@ class PostgresqlDB(DatabaseStored):
         text(sql_create_database, metadata.bind).execute(
             database=database_name)
         #text(sql_revoke_permissions, metadata.bind).execute(user = user_name)
-        connection.close()
+        metadata.bind.engine.dispose()
+        #connection.close()
         metadata.bind.engine.dispose()
         # Restores the old database
         metadata.bind = bind
@@ -155,9 +160,10 @@ class PostgresqlDB(DatabaseStored):
         # Creates a connection with permission to create users and databases
         metadata.bind = self.__generate_connection_uri(
             self.__root_user, self.__root_password)
-        connection = metadata.bind.engine.connect()
-        connection.connection.connection.set_isolation_level(0)
-
+        #connection = metadata.bind.engine.connect()
+        #connection.connection.connection.set_isolation_level(0)
+        metadata.bind.engine.connect().\
+            connection.connection.set_isolation_level(0)
         # Generate data and queries
         sql_delete_database = "DROP DATABASE " + database_name + ";"
         sql_delete_user = "DROP USER " + user_name + ";"
@@ -165,7 +171,7 @@ class PostgresqlDB(DatabaseStored):
         text(sql_delete_database, metadata.bind).execute(
             database=database_name)
         text(sql_delete_user, metadata.bind).execute(user=user_name)
-        connection.close()
+        #connection.close()
         metadata.bind.engine.dispose()
         # Restores the old database_name
         metadata.bind = bind