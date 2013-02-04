139c7ecd06da8796e6014ea08b45edc9e4dfbe05
David Navarro <davisein@gmail.com>
2013-02-04 16:36:09 +0100

    [photos] pep8-ify model

--- apps/app.photos/server-python/src/nublic_app_photos_server/model.py
+++ apps/app.photos/server-python/src/nublic_app_photos_server/model.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 # Create database to base the model
 db = SQLAlchemy()
 
+
 class Photo(db.Model):
     __tablename__ = 'Photo'
 
@@ -25,17 +26,21 @@ class Photo(db.Model):
     def __repr__(self):
         return '<Photo %r "%r" at %r>' % (self.id, self.title, self.file)
 
+
 def photo_by_filename(filename):
     Photo.query.filter_by(file=filename).first()
 
+
 def photo_as_json(photo):
-    return { 'id': photo.id,
-             'title': photo.title,
-             'date': int(photo.date.strftime('%s'))
-           }
+    return {'id': photo.id,
+            'title': photo.title,
+            'date': int(photo.date.strftime('%s'))
+            }
+
 
 def photos_and_row_count_as_json(row_count, photos):
-    return { 'row_count': row_count, 'photos': map(photo_as_json, photos) }
+    return {'row_count': row_count, 'photos': map(photo_as_json, photos)}
+
 
 class Album(db.Model):
     __tablename__ = 'Album'
@@ -49,17 +54,20 @@ class Album(db.Model):
     def __repr__(self):
         return '<Album %r "%r">' % (self.id, self.name)
 
+
 def album_by_name(album_name):
     Album.query.filter_by(name=album_name).first()
 
+
 def album_as_json(album):
-    return { 'id': album.id,
-             'name': album.name
-           }
+    return {'id': album.id,
+            'name': album.name
+            }
+
 
 def get_or_create_album(album_name):
     ab = album_by_name(album_name)
-    if ab == None:
+    if ab is None:
         ab = Album(album_name)
         db.session.add(ab)
         db.session.commit()
@@ -67,11 +75,14 @@ def get_or_create_album(album_name):
     else:
         return ab
 
+
 class PhotoAlbum(db.Model):
     __tablename__ = 'PhotoAlbum'
 
-    photoId = db.Column('photoId', db.BigInteger, db.ForeignKey('Photo.id'), primary_key=True)
-    albumId = db.Column('albumId', db.BigInteger, db.ForeignKey('Album.id'), primary_key=True)
+    photoId = db.Column(
+        'photoId', db.BigInteger, db.ForeignKey('Photo.id'), primary_key=True)
+    albumId = db.Column(
+        'albumId', db.BigInteger, db.ForeignKey('Album.id'), primary_key=True)
 
     def __init__(self, albumId, photoId):
         self.photoId = photoId
