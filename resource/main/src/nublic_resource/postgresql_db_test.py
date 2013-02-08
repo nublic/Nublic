c3c93acfeb5da6a024699ef64247dcf942505d68
David Navarro <davisein@gmail.com>
2013-02-08 18:33:00 +0100

    [resource] Fix warnings and formatting

--- resource/main/src/nublic_resource/postgresql_db_test.py
+++ resource/main/src/nublic_resource/postgresql_db_test.py
@@ -5,16 +5,15 @@ Created on 06/09/2011
 '''
 import unittest
 from nublic_resource.postgresql_db import PostgresqlDB
-from nublic_resource.database_stored import ExistingKeyError
+#from nublic_resource.database_stored import ExistingKeyError
 from nublic_resource import database_stored
+
 
 class PostgresqlDBTest(unittest.TestCase):
 
-        
     def setUp(self):
-        self.postgresql = PostgresqlDB() 
+        self.postgresql = PostgresqlDB()
         pass
-
 
     def tearDown(self):
         try:
@@ -22,36 +21,34 @@ class PostgresqlDBTest(unittest.TestCase):
         except Exception:
             pass
 
-
     def testName(self):
         pass
-    
+
     def testProviderType(self):
-        self.assertEqual(self.postgresql.providerType, 'postgresql-db', 
-                         "Wrong provider id")  
+        self.assertEqual(self.postgresql.providerType, 'postgresql-db',
+                         "Wrong provider id")
 
     def testInstall(self):
         self.postgresql.install("Test_app", "test")
-        
+
         # Test no double installation
         with self.assertRaises(database_stored.ExistingKeyError):
             self.postgresql.install("Test_app", "test")
 
-        # Test all values are retrievable 
+        # Test all values are retrievable
         user = self.postgresql.value("Test_app", "test", "user")
-        self.assertIsNotNone(user, "User for postgresql not available with value")
+        self.assertIsNotNone(
+            user, "User for postgresql not available with value")
         password = self.postgresql.value("Test_app", "test", "pass")
-        self.assertIsNotNone(password, 
+        self.assertIsNotNone(password,
                              "pass for postgresql not available with value")
         database = self.postgresql.value("Test_app", "test", "database")
-        self.assertIsNotNone(database, 
+        self.assertIsNotNone(database,
                              "database for postgresql not available with value")
 
         self.assertEquals(self.postgresql.value("Test_app", "test", "uri"),
-                          "postgresql://" + user + ":" + password + \
+                          "postgresql://" + user + ":" + password +
                           "@localhost/" + database)
         # @ŧodo Connect to the database to see if it works
         self.postgresql.uninstall("Test_app", "test")
         # @todo Try to connect to the database to see that it doesn't work
-
-    
--- resource/main/src/nublic_resource/provider.py
+++ resource/main/src/nublic_resource/provider.py
@@ -4,9 +4,11 @@ Created on 10/08/2010
 @author: David Navarro Estruch
 @copyright: 2011 Nublic
 '''
-from elixir import setup_all, session
+#from elixir import setup_all, session
 
 from model import App, Key
+
+
 class Provider(object):
     '''
     classdocs
@@ -22,24 +24,24 @@ class Provider(object):
         '''
         Must perform a install over the given app, key.
         Optionally can have a number of ordered args.
-        
+
         @param app: string The AppId
         @param key: string The wanted key to identify in the future this resource
         @param args: list Optional args
-        
+
         Raises RepeatedKeyError
         '''
         raise NotImplementedError()
-    
+
     def uninstall(self, app, key):
         '''
         Must perform a uninstall over the given app, key.
         Optionally can have a number of ordered args.
-        
+
         @param app: string The AppId
         @param key: string The wanted key to uninstall
         @param args: list Optional args
-        
+
         @raise NotExistingKeyError
         '''
         raise NotImplementedError()
@@ -48,36 +50,36 @@ class Provider(object):
         '''
         Must perform a request over the given app, key.
         Optionally can have a number of ordered args.
-        
+
         @param app: string The AppId
         @param key: string The wanted key to identify in the future this request
         @param args: list Optional args
-        
+
         Raises RepeatedKeyError
         '''
         raise NotImplementedError()
-    
+
     def release(self, app, key):
         '''
         Must perform a release over the given app, key.
         Optionally can have a number of ordered args.
-        
+
         @param app: string The AppId
         @param key: string The wanted key to release
         @param args: list Optional args
-        
+
         @raise NotExistingKeyError
         '''
         raise NotImplementedError()
-        
+
     def value(self, app, key, subkey):
         '''
         Returns the value of the given pair key/subkey.
-        
+
         @param app: string The AppId
         @param key: string The key used to Request
         @param subkey string The subkey wanted. If None return the None subkey.
-        
+
         @return: string value asked
         @raise NotExistingKeyError: Contains the key or subkey that does not exist.
         '''
@@ -89,21 +91,25 @@ class Provider(object):
 
         @param app: string The AppId
         @param key: string The key used to Request
-        
+
         @return: List of string with the available subkeys
         '''
         raise NotImplementedError()
 
+
 def get_all_apps():
     return App.query.all()
-    
+
+
 def get_all_keys():
     return Key.query.all()
-            
+
+
 class ReapeatedKeyError(Exception):
     def __init__(self, key):
         self.key = key
-        
+
+
 class NotExistingKeyError(Exception):
     def __init__(self, key):
         self.key = key
