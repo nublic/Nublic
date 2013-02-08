183132fc66ccd4569047edf613c68a81c0cbd447
David Navarro <davisein@gmail.com>
2013-02-08 13:11:00 +0100

    [python] Unicode + logs + pep8 support

--- apps/app.manager/server-python/src/nublic_app_manager_server/server.py
+++ apps/app.manager/server-python/src/nublic_app_manager_server/server.py
@@ -5,7 +5,7 @@ import os.path
 import random
 import simplejson as json
 import string
-import pexpect
+#import pexpect
 
 from nublic.files_and_users import get_all_users, Mirror, WorkFolder, create_mirror, create_work_folder
 from nublic_server.helpers import init_bare_nublic_server, require_user
@@ -18,6 +18,7 @@ app.logger.error('Starting manager app')
 
 APP_INFO_ROOT = '/var/lib/nublic/apps'
 
+
 def load_apps():
     apps = dict()
     for f in os.listdir(APP_INFO_ROOT):
@@ -28,6 +29,7 @@ def load_apps():
             apps[app_data['id']] = app_data
     return apps
 
+
 @app.route('/apps')
 def apps():
     apps = load_apps()
@@ -35,10 +37,11 @@ def apps():
     for app_id in apps:
         current_app = apps[app_id]
         if 'web' in current_app:
-            return_app = { 'id': current_app['id'], 'name': current_app['name'],
-                           'developer': current_app['developer'], 'web': current_app['web'] }
+            return_app = {'id': current_app['id'], 'name': current_app['name'],
+                          'developer': current_app['developer'], 'web': current_app['web']}
             return_apps.append(return_app)
     return json.dumps(return_apps)
+
 
 @app.route('/app-image/<style>/<app_id>/<size>')
 def app_image(style, app_id, size):
@@ -55,37 +58,42 @@ def app_image(style, app_id, size):
     else:
         abort(404)
 
+
 @app.route('/users')
 def users():
     return_users = []
     for user in get_all_users():
-        return_user = { 'username': user.get_username(),
-                        'uid': user.get_id(),
-                        'name': user.get_shown_name() }
+        return_user = {'username': user.get_username(),
+                       'uid': user.get_id(),
+                       'name': user.get_shown_name()}
         return_users.append(return_user)
     return json.dumps(return_users)
+
 
 @app.route('/user-name')
 def user_name():
     user = require_user()
     return user.get_shown_name()
 
+
 @app.route('/user-info', methods=['GET'])
 def user_info_get():
     user = require_user()
-    return json.dumps({ 'username': user.get_username(),
-                            'uid': user.get_id(),
-                            'name': user.get_shown_name() })
+    return json.dumps({'username': user.get_username(),
+                       'uid': user.get_id(),
+                       'name': user.get_shown_name()})
+
 
 @app.route('/user-info', methods=['PUT'])
 def user_info_put():
     user = require_user()
     name = request.form.get('name', None)
-    if name != None:
+    if name is not None:
         user.change_shown_name(name)
         return 'ok'
     else:
         abort(400)
+
 
 @app.route('/mirrors', methods=['GET'])
 def mirrors_get():
@@ -93,21 +101,23 @@ def mirrors_get():
     # Get mirrors information
     return_mirrors = []
     for mirror in user.get_owned_mirrors():
-        return_mirror = { 'id': mirror.get_id(), 'name': mirror.get_name() }
+        return_mirror = {'id': mirror.get_id(), 'name': mirror.get_name()}
         return_mirrors.append(return_mirror)
     return_mirrors.sort(key=lambda m: m['name'])
     return json.dumps(return_mirrors)
+
 
 @app.route('/mirrors', methods=['PUT'])
 def mirrors_put():
     user = require_user()
     # Create a new mirror
     name = request.form.get('name', None)
-    if name != None:
+    if name is not None:
         mirror = create_mirror(name, user.get_username())
-        return str(mirror.get_id())
+        return unicode(mirror.get_id())
     else:
         abort(400)
+
 
 @app.route('/mirrors', methods=['DELETE'])
 def mirrors_delete():
@@ -121,18 +131,20 @@ def mirrors_delete():
     else:
         abort(403)
 
+
 @app.route('/mirror-name', methods=['PUT'])
 def mirror_name():
     user = require_user()
     name = request.form.get('name', None)
     mid = int(request.form.get('id'))
     m = Mirror(mid)
-    if name != None and m.exists() and m.get_owner().get_username() == user.get_username():
+    if name is not None and m.exists() and m.get_owner().get_username() == user.get_username():
         # Change name
         m.change_name(name)
         abort(200)
     else:
         abort(403)
+
 
 @app.route('/synceds', methods=['GET'])
 def synceds_get():
@@ -140,24 +152,27 @@ def synceds_get():
     # Get mirrors information
     return_synceds = []
     for synced in user.get_owned_work_folders():
-        return_synced = { 'id': synced.get_id(), 'name': synced.get_name() }
+        return_synced = {'id': synced.get_id(), 'name': synced.get_name()}
         return_synceds.append(return_synced)
     return_synceds.sort(key=lambda m: m['name'])
     return json.dumps(return_synceds)
+
 
 @app.route('/synceds', methods=['PUT'])
 def synceds_put():
     user = require_user()
     # Create a new mirror
     name = request.form.get('name', None)
-    if name != None:
+    if name is not None:
         synced = create_work_folder(name, user.get_username())
-        return str(synced.get_id())
+        return unicode(synced.get_id())
     else:
         abort(400)
 
+
 @app.route('/synceds', methods=['DELETE'])
 def synceds_delete():
+    user = require_user()
     # Delete a mirror
     sid = int(request.form.get('id'))
     s = WorkFolder(sid)
@@ -167,13 +182,14 @@ def synceds_delete():
     else:
         abort(403)
 
+
 @app.route('/synced-name', methods=['PUT'])
 def synced_name():
     user = require_user()
     name = request.form.get('name', None)
     sid = int(request.form.get('id'))
     s = WorkFolder(sid)
-    if name != None and s.exists() and s.get_owner().get_username() == user.get_username():
+    if name is not None and s.exists() and s.get_owner().get_username() == user.get_username():
         # Change name
         s.change_name(name)
         abort(200)
@@ -183,27 +199,33 @@ def synced_name():
 # Work on SSH keys
 current_upload_keys = []
 
+
 def prune_old_upload_keys():
     global current_upload_keys
     five_minutes = datetime.timedelta(minutes=5)
     five_minutes_before_now = datetime.datetime.now() - five_minutes
-    current_upload_keys = filter(lambda x: x['time'] > five_minutes_before_now, current_upload_keys)
+    current_upload_keys = filter(
+        lambda x: x['time'] > five_minutes_before_now, current_upload_keys)
+
 
 @app.route('/synced-generate-invite/<int:sid>')
 def synced_generate_invite(sid):
     user = require_user()
     s = WorkFolder(sid)
-    #app.logger.error('User: %s, existance: %s, owner: %s', user.get_username(), str(s.exists()), s.get_owner().get_username())
+    app.logger.info('User: %s, existance: %s, owner: %s', user.get_username(
+    ), unicode(s.exists()), s.get_owner().get_username())
     if s.exists() and s.get_owner().get_username() == user.get_username():
         random_id = random.randint(100, 2000000)
-        current_upload_keys.append({ 'id': random_id, 'time': datetime.datetime.now(), 'synced_id': sid, 'user': user })
-        return str(random_id)
+        current_upload_keys.append({'id': random_id, 'time': datetime.datetime.now(), 'synced_id': sid, 'user': user})
+        return unicode(random_id)
     else:
         abort(403)
 
+
 def get_fingerprint():
     f = open('/var/nublic/cache/fingerprint')
-    return f.read().replace("\n","")
+    return f.read().replace("\n", "")
+
 
 @app.route('/synced-invite/<int:iid>')
 def synced_invite(iid):
@@ -214,10 +236,10 @@ def synced_invite(iid):
         s = WorkFolder(r_invite['synced_id'])
         user = r_invite['user']
         if s.exists() and s.get_owner().get_username() == user.get_username():
-            fingerprint = get_fingerprint() 
+            fingerprint = get_fingerprint()
             manager_path, _ = os.path.split(request.url_root[:-1])
             server = os.path.split(manager_path)[0].replace('http://', '')
-            server = server.replace('https://','')
+            server = server.replace('https://', '')
             xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + \
                   "<sparkleshare>\n" + \
                   "  <invite>\n" + \
@@ -230,6 +252,7 @@ def synced_invite(iid):
             return xml
     else:
         abort(403)
+
 
 @app.route('/synced-upload-key/<int:iid>', methods=['POST'])
 def synced_upload_key(iid):
@@ -245,12 +268,14 @@ def synced_upload_key(iid):
         ssh_initial = token_list[0]
         ssh_name = token_list[-1]
         ssh_rest = token_list[1:-1]
-        real_key = ssh_initial + ' ' + string.join(ssh_rest, '+') + ' ' + ssh_name
+        real_key = ssh_initial + ' ' + string.join(ssh_rest, '+') + \
+            ' ' + ssh_name
         # Finally add key
         user.add_public_key(real_key)
     else:
         abort(403)
-        
+
+
 @app.route('/change-password', methods=['POST'])
 def change_password():
     user = require_user()
