90391df1a0b1e506496b52288f1f3c101cadea27
Pablo Tuñón <pablo@nublic.com>
2013-02-08 16:45:50 +0100

    [app.manager.welcome] Improved notification system. Added hack trials.

---
+++ apps/app.manager/welcome/src/main/java/com/nublic/app/manager/welcome/client/notifications/NotificationHackManager.java
@@ -0,0 +1,54 @@
+package com.nublic.app.manager.welcome.client.notifications;
+
+import java.util.ArrayList;
+
+import com.google.gwt.safehtml.shared.SafeHtml;
+
+// Class created to mock the behaviour of a notification system
+// Shouldn't exist in final version
+public class NotificationHackManager {
+	public static ArrayList<Notification> getNotificationsOf(String id) {
+		ArrayList<Notification> retList = new ArrayList<Notification>();
+
+		if (id.compareTo("browser") == 0) {
+		} else if (id.compareTo("photos") == 0) {
+			retList.add(new Notification(NotificationType.ATTENTION, new SafeHtml() {
+				private static final long serialVersionUID = 4654149898045281890L;
+
+				@Override
+				public String asString() {
+					return "Se eliminó el álbum <span style=\"color: red\">erasmus</span>";
+				}
+			}));
+			retList.add(new Notification(new SafeHtml() {
+				private static final long serialVersionUID = 4654149898045281890L;
+
+				@Override
+				public String asString() {
+					return "Añadidas 3 fotos al álbum <a href=\"\">vacaciones</a>";
+				}
+			}));
+		} else if (id.compareTo("music") == 0) {
+			retList.add(new Notification(new SafeHtml() {
+				private static final long serialVersionUID = -482079151180273389L;
+
+				@Override
+				public String asString() {
+					return "Hay 33 nuevas canciones de <a href=\"\">The Beatles</a>, <a href=\"\">Muse</a> y <a href=\"\">Queen</a> disponibles en el reproductor de música";
+				}
+			}));
+		} else if (id.compareTo("house") == 0) {
+			retList.add(new Notification(NotificationType.POWER, new SafeHtml() {
+				private static final long serialVersionUID = 4654149898045281890L;
+
+				@Override
+				public String asString() {
+					return "Programado  <a href=\"\">apagado de luces</a> en <a href=\"\">salón</a> a las <strong>23:30</strong>";
+				}
+			}));
+		} else {
+			retList.add(new Notification());
+		}
+		return retList;
+	}
+}
--- apps/app.manager/welcome/src/main/java/com/nublic/app/manager/welcome/client/notifications/NotificationLine.java
+++ apps/app.manager/welcome/src/main/java/com/nublic/app/manager/welcome/client/notifications/NotificationLine.java
@@ -1,14 +1,18 @@
-package com.nublic.app.manager.welcome.client;
+package com.nublic.app.manager.welcome.client.notifications;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ImageResource;
+import com.google.gwt.safehtml.shared.SafeHtml;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.DeckPanel;
+import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.HasText;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
+import com.nublic.app.manager.welcome.client.Resources;
 
 public class NotificationLine extends Composite implements HasText {
 	private static NotificationLineUiBinder uiBinder = GWT.create(NotificationLineUiBinder.class);
@@ -16,6 +20,7 @@ public class NotificationLine extends Composite implements HasText {
 	
 	@UiField Image icon;
 	@UiField Label text;
+	@UiField DeckPanel notiPanel;
 
 	public NotificationLine() {
 		initWidget(uiBinder.createAndBindUi(this));
@@ -23,6 +28,22 @@ public class NotificationLine extends Composite implements HasText {
 		// set default notification
 		setIcon(Resources.INSTANCE.info());
 		setText("Notification");
+		notiPanel.showWidget(0);
+	}
+	
+	public NotificationLine(Notification n) {
+		initWidget(uiBinder.createAndBindUi(this));
+
+		// set n notification
+		setIcon(n.getIcon());
+		setText(n.getText());
+		SafeHtml html = n.getHtml();
+		if (html != null) {
+			notiPanel.add(new HTMLPanel(html));
+			notiPanel.showWidget(1);
+		} else {
+			notiPanel.showWidget(0);
+		}
 	}
 	
 	public void setIcon(ImageResource ir) {
