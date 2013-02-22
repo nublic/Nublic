5bf87ccfb4b06cab53fe37dac400b7e37a686a08
Pablo Tuñón <pablo@nublic.com>
2013-02-22 15:29:54 +0100

    [app.manager.settings] Created PassWithViewer

--- apps/app.manager/settings/src/main/java/com/nublic/app/manager/settings/client/ui/MasterPassWidget.java
+++ apps/app.manager/settings/src/main/java/com/nublic/app/manager/settings/client/ui/MasterPassWidget.java
@@ -4,14 +4,14 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.PasswordTextBox;
 import com.google.gwt.user.client.ui.Widget;
+import com.nublic.app.manager.settings.client.ui.password.PassWithViewer;
 
 public class MasterPassWidget extends Composite {
 	private static MasterPassWidgetUiBinder uiBinder = GWT.create(MasterPassWidgetUiBinder.class);
 	interface MasterPassWidgetUiBinder extends UiBinder<Widget, MasterPassWidget> {	}
 
-	@UiField PasswordTextBox passBox;
+	@UiField PassWithViewer passBox;
 	
 	public MasterPassWidget() {
 		initWidget(uiBinder.createAndBindUi(this));
