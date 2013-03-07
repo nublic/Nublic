32cabc2428d42481f33074ae8c667f364578058a
David Navarro Estruch <davisein@gmail.com>
2013-03-07 23:12:48 +0100

    [rpcbd_client] Cache client sockets

--- python/client/src/nublic/rpcbd_client.py
+++ python/client/src/nublic/rpcbd_client.py
@@ -1,14 +1,25 @@
 from rpcbd import ThreadedTCPJsonRpcPeer, JSONRPC_V2
 
-def rpcbd_call_return(port, f):
-    peer = ThreadedTCPJsonRpcPeer(JSONRPC_V2)
-    connection = peer.connect_tcp('127.0.0.1', port)
+peer = None
+connection = None
+
+
+def rpcbd_call_return(port, f, timeout=10):
+    global peer
+    global connection
+    if not peer or not connection:
+        peer = ThreadedTCPJsonRpcPeer(JSONRPC_V2, default_timeout=timeout)
+        connection = peer.connect_tcp('127.0.0.1', port)
     data = f(connection.request)
-    peer.shutdown()
+    #peer.shutdown()
     return data
 
-def rpcbd_call(port, f):
-    peer = ThreadedTCPJsonRpcPeer(JSONRPC_V2)
-    connection = peer.connect_tcp('127.0.0.1', port)
+
+def rpcbd_call(port, f, timeout=10):
+    global peer
+    global connection
+    if not peer or not connection:
+        peer = ThreadedTCPJsonRpcPeer(JSONRPC_V2, default_timeout=timeout)
+        connection = peer.connect_tcp('127.0.0.1', port)
     f(connection.request)
-    peer.shutdown()
+    #peer.shutdown()
